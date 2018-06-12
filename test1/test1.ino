f39e273daa103b672f8f5d32d642d6200eed5c50
DrakLulu <charlotte.madroux@gmail.com>
2018-06-12 13:22:50 +0200

    Ajout des capteurs
    Gestion de la table logigue des capteurs suiveur de ligne.

--- test1/test1.ino
+++ test1/test1.ino
@@ -3,14 +3,19 @@
 //MOTOR1 = DROITE
 // default I2C address is 0x0f
 #define I2C_ADDRESS 0x0f
+#define LRi 1 //left RI sensor
+#define LRI 2 //left RI sensor line follower
+#define RRI 3 //right RI sensor line follower
+#define RRi 4 //right RI sensor
+
 
 void forward (int x)
 {
-
  Motor.speed(MOTOR1, 50);
   // Set speed of MOTOR2, Anticlockwise
   Motor.speed(MOTOR2, 50);
   delay(x);
+  stopMotor(); 
 }
 
 void turnRightCorner(){
@@ -20,6 +25,7 @@ void turnRightCorner(){
   Motor.speed(MOTOR2, 50);
   delay(1000);
   // Stop MOTOR1 and MOTOR2
+  stopMotor();
 }
 
 void turnLeftCorner(){
@@ -29,6 +35,62 @@ void turnLeftCorner(){
   Motor.speed(MOTOR2, -50);
   delay(1000);
   // Stop MOTOR1 and MOTOR2
+  stopMotor(); 
+}
+
+
+void stopMotor (){
+  Motor.stop(MOTOR1);
+  Motor.stop(MOTOR2);
+}
+
+
+void getsensor(){
+  int lri;
+  int rri;
+
+  if (digitalRead(LRI) == HIGH){
+     lri = 1;
+  }
+  else {lri = 0;}
+
+  if (digitalRead(RRI) == HIGH){
+     rri = 1;
+  }
+  else {rri = 0;}
+
+  driving(lri, rri);
+
+}
+
+void rectifyright(){
+
+}
+
+void rectifyleft(){
+
+}
+
+
+void driving(int lri, int rri){
+  switch(lri){
+
+      case 1:
+      if(rri = 1){
+        forward(200);
+      }else {rectifyright();} // TO DO: CODE rectifyright()
+      break;
+
+      case 0: 
+      if(rri == 1){
+        rectifyleft();  // TO DO: CODE rectifyleft()
+      } else {stopMotor();}
+      break;
+
+      default:
+      stopMotor(); 
+      break; 
+      }
 }
 
 void setup() {
@@ -40,8 +102,6 @@ void loop() {
   forward(2000);
   turnRightCorner();
   turnLeftCorner();
-  Motor.stop(MOTOR1);
-  Motor.stop(MOTOR2);
-  
-}
+  stopMotor(); 
 
+}
