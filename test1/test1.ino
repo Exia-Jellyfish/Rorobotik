197720573445b7219a79a7c0348c97a7c6574e08
Exia-Jellyfish <33414884+Exia-Jellyfish@users.noreply.github.com>
2018-06-12 11:32:48 +0200

    arduino déplacment
    forward + turn around

---
+++ test1/test1.ino
@@ -0,0 +1,35 @@
+#include "Grove_I2C_Motor_Driver.h"
+
+// default I2C address is 0x0f
+#define I2C_ADDRESS 0x0f
+
+void forward (int x)
+{
+
+ Motor.speed(MOTOR1, 50);
+  // Set speed of MOTOR2, Anticlockwise
+  Motor.speed(MOTOR2, 50);
+  delay(x);
+}
+
+void turncorner(){
+  // Change speed and direction of MOTOR1
+  Motor.speed(MOTOR1, -50);
+  // Change speed and direction of MOTOR2
+  Motor.speed(MOTOR2, 50);
+  delay(1000);
+  // Stop MOTOR1 and MOTOR2
+}
+void setup() {
+  Motor.begin(I2C_ADDRESS);
+}
+
+void loop() {
+  delay(2000);
+  forward(2000);
+  turncorner();
+  Motor.stop(MOTOR1);
+  Motor.stop(MOTOR2);
+  
+}
+
