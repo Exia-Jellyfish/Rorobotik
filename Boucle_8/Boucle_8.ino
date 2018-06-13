d6c5183e7df9ff8329f5e38f15bcb0bdada95256
VincentOnfray <33415283+VincentOnfray@users.noreply.github.com>
2018-06-13 13:01:06 +0200

    ajout du code etats

--- Boucle_8/Boucle_8.ino
+++ Boucle_8/Boucle_8.ino
@@ -1,14 +1,13 @@
 #include "Grove_I2C_Motor_Driver.h"
-//MOTOR2 = GAUCHE
-//MOTOR1 = DROITE
+//MOTOR1 = GAUCHE
+//MOTOR2 = DROITE
 // default I2C address is 0x0f
 #define I2C_ADDRESS 0x0f
 
 void forward (int x)
 {
-
- Motor.speed(MOTOR1, -50);
-  // Set speed of MOTOR2, Anticlockwise
+  Motor.speed(MOTOR1, -50);
+  // Set speed of MOTOR2, Clockwise
   Motor.speed(MOTOR2, -50);
   delay(x);
   Motor.stop(MOTOR1);
@@ -44,26 +43,26 @@ void setup() {
 }
 
 void loop() {
-  
-  forward(1000);
+  //This code orders the robot to do an "s" 
+  forward(500);
   turnLeftCorner();
-  forward(2000);
+  forward(1000);
    turnLeftCorner();
-  forward(2000);
+  forward(1000);
  turnLeftCorner();;
-  forward(2000);
- turnLeftCorner();m
-  forward(2000);
-  turnRightCorner();
-  forward(2000);
-  turnRightCorner();
-  forward(2000);
-  turnRightCorner();
-  forward(2000);
-  turnRightCorner();
-  forward(2000);
+  forward(1000);
+ turnLeftCorner();
+  forward(1000);
   turnRightCorner();
   forward(1000);
+  turnRightCorner();
+  forward(1000);
+  turnRightCorner();
+  forward(1000);
+  turnRightCorner();
+  forward(1000);
+  turnRightCorner();
+  forward(500);
   delay(5000);
 }
 
