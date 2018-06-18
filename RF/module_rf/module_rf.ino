4f785a80d9842c9729a3954c70f2b6f724e8fff8
romainmonnery <33548896+romainmonnery@users.noreply.github.com>
2018-06-18 15:04:36 +0100

    test rf fonctiuonnel

--- RF/module_rf/module_rf.ino
+++ RF/module_rf/module_rf.ino
@@ -1,9 +1,30 @@
-void setup() {
-  // put your setup code here, to run once:
+#include <VirtualWire.h>
 
-}
+    int RF_RX_PIN = 6;
 
-void loop() {
-  // put your main code here, to run repeatedly:
+    void setup()
+    {
+      Serial.begin(9600);
+      vw_set_rx_pin(RF_RX_PIN);  // Setup receive pin.
+      vw_setup(2000); // Transmission speed in bits per second.
+      vw_rx_start(); // Start the PLL receiver.
+      Serial.print("ready");
+    }
 
-}
+    void loop()
+    {
+      uint8_t buf[VW_MAX_MESSAGE_LEN];
+      uint8_t buflen = VW_MAX_MESSAGE_LEN;
+      if(vw_get_message(buf, &buflen)) // non-blocking I/O
+      {
+        int i;
+        // Message with a good checksum received, dump HEX
+        Serial.print("Got: ");
+        for(i = 0; i < buflen; ++i)
+        {
+          Serial.print(buf[i], HEX);
+          Serial.print(" ");
+        }
+        Serial.println("");
+      }
+    }
