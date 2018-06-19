180373c78ef9dc81b85f39ba15d0628476ca3f60
DrakLulu <charlotte.madroux@gmail.com>
2018-06-19 10:22:11 +0200

    Liaison RF
    Ajout du protocole de communication.
    WIP

--- RF/envoi/envoi.ino
+++ RF/envoi/envoi.ino
@@ -1,15 +1,68 @@
 #include <VirtualWire.h>
+#include <stdint.h>
 
 int RF_TX_PIN = 7;
+int n = 1;
+uint8_t checkSum; 
+
+struct entete{
+  const uint8_t start = "$";
+  const uint8_t recepter = "RobotGr1";
+  const uint8_t emetter = "EmettGr1";
+  uint8_t idTram;
+  uint8_t checksum;
+};
+
+struct data{
+  uint16_t data = "Hello recepteur!"; 
+};
+
+struct packet{
+  entete head;
+  data data; 
+};
+
+uint16_t creatPacket(){
+
+  entete hEad;
+  data dAta;  
+  strcpy(hEad.idTram, n );
+  n++;
+  unsigned char *daTa = "Hello recepteur!";
+  strcpy(dAta.data, daTa);
+  uint16_t checksum = chksum8(daTa, 16);
+  strcpy(hEad.checksum, checkSum);
+  packet pcket = {hEad, dAta};
+  
+  uint16_t crypted = (hEad.start + hEad.recepter + hEad.emetter + n + hEad.checksum + dAta.data) ;
+ return crypted; 
+}
+
+uint16_t chksum8(unsigned char *daTa, size_t len)
+{
+    unsigned int sum;       
+    for ( sum = 0 ; len != 0 ; len-- ){
+        sum += *daTa++;  
+        Serial.print(sum);
+        }
+    return (uint8_t)sum;
+}
+
+
+
+
+
 void setup()
 {
  vw_set_tx_pin(RF_TX_PIN); // Setup transmit pin
  vw_setup(2000); // Transmission speed in bits per second.
+ struct packet packet; 
 }
 
 void loop()
 {
  const char *msg = "Hello, World!";
- vw_send((uint8_t *)msg, strlen(msg));  // Send 'hello' every 400ms.
+uint16_t pckt = creatPacket();
+ vw_send((uint8_t *)pckt, strlen(pckt));  
  delay(100);
 }
--- RF/module_rf/module_rf.ino
+++ RF/module_rf/module_rf.ino
@@ -1,6 +1,47 @@
 #include <VirtualWire.h>
 
     int RF_RX_PIN = 6;
+    int n = 1;
+    uint8_t checkSum; 
+    uint8_t buf[VW_MAX_MESSAGE_LEN];
+    uint8_t buflen = VW_MAX_MESSAGE_LEN;
+    
+    struct entete{
+      uint8_t start;
+      uint8_t recepter;
+      uint8_t emetter;
+      uint8_t idTram;
+      uint8_t checksum;
+    };
+    
+    struct data{
+      uint16_t data; 
+    };
+    
+    struct packet{
+      entete head;
+      data data; 
+    };
+
+
+    void affichermsg (){
+     
+      vw_wait_rx();
+      if(vw_get_message(buf, &buflen)) // non-blocking I/O
+      {
+        int i;
+        // Message with a good checksum received, dump HEX
+        Serial.print("Got: ");
+        for(i = 0; i < buflen; ++i)
+        {
+          Serial.print((char)buf[i]); 
+        }
+        Serial.println("");
+      }
+      
+    }
+
+    
 
     void setup()
     {
@@ -13,20 +54,6 @@
 
     void loop()
     {
-      uint8_t buf[VW_MAX_MESSAGE_LEN];
-      uint8_t buflen = VW_MAX_MESSAGE_LEN;
-       vw_wait_rx();
-      if(vw_get_message(buf, &buflen)) // non-blocking I/O
-      {
-        int i;
-        // Message with a good checksum received, dump HEX
-        Serial.print("Got: ");
-        for(i = 0; i < buflen; ++i)
-        {
-          //Serial.print(buf[i], HEX);
-          //Serial.print(" ");
-          Serial.print((char)buf[i]); 
-        }
-        Serial.println("");
-      }
+      
+      affichermsg(); 
     }
