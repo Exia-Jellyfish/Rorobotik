03841a091fc355192f4041cdf80c65d6944c01db
DrakLulu <charlotte.madroux@gmail.com>
2018-06-22 12:54:12 +0200

    Module RF envoi Ok
    l'envois avec le protocole se fait.
    reception à 5cm. Problématiques matérielles.
    
    Mise en place d'un début de protocole côté recepteur.

--- RF/module_rf/module_rf.ino
+++ RF/module_rf/module_rf.ino
@@ -1,6 +1,7 @@
 #include <VirtualWire.h>
 
-    int RF_RX_PIN = 2;
+    int RF_RX_PIN = 8;
+   
 
     void setup()
     {
@@ -8,7 +9,7 @@
       vw_set_rx_pin(RF_RX_PIN);  // Setup receive pin.
       vw_setup(2000); // Transmission speed in bits per second.
       vw_rx_start(); // Start the PLL receiver.
-      Serial.println("ready");
+      Serial.println("ready recepteur");
     }
 
     void loop()
@@ -16,6 +17,7 @@
       uint8_t buf[VW_MAX_MESSAGE_LEN];
       uint8_t buflen = VW_MAX_MESSAGE_LEN;
       //vw_wait_rx();
+   
       if(vw_get_message(buf, &buflen)) // non-blocking I/O
       {
         int i;
@@ -23,9 +25,52 @@
         Serial.print("Got: ");
         for(i = 0; i < buflen; ++i)
         {
-          Serial.print((char *) buf[i]);
+          Serial.print((char)buf[i]);
         }
         Serial.println("");
-      } else Serial.println("else");
-       //Serial.println("sortie de boucle"); 
+      } 
+      
     }
+
+
+
+  /*  void lecturemsg(){
+      uint8_t buf[VW_MAX_MESSAGE_LEN];
+      uint8_t buflen = VW_MAX_MESSAGE_LEN;
+      char *message = "";
+      char *chsum;
+      char *checksum;
+
+      if (buf[3] = "?"){
+         if(buf[4] = "g" && buf[5] ="1"){
+            if(buf[6] = "e" && buf(7] = "1"){
+              for(int i = 11; i < buflen; i++){
+              strcat(message, (char *)buf[i]);
+              }
+              chsum = getCheckSum(char *message);
+                for(int i = 8; i < 11; i++){
+                strcat(checksum, (char *)buf[i]);
+                }
+                  if(checksum == chsum){
+                    
+                  }
+            }    
+         }
+        
+            
+      }
+    }
+
+    char * getCheckSum(char *string)
+{
+  int XOR;  
+  for (int i = 0; i < strlen(string); i++) 
+  {
+    XOR += *(string++); 
+  }
+  char xOR[12];
+  sprintf(xOR, "%d", XOR);
+  return xOR; 
+
+}*/
+
