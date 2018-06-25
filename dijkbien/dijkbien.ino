66203c65fda39e0b9cc41a7aad8a84c05861371d
DrakLulu <charlotte.madroux@gmail.com>
2018-06-25 09:39:41 +0200

    Re merge de Dijkstra et RF

--- dijkbien/dijkbien.ino
+++ dijkbien/dijkbien.ino
@@ -1,6 +1,4 @@
-
-
-
+#include <VirtualWire.h>
 #include "Grove_I2C_Motor_Driver.h"
 
 #define I2C_ADDRESS 0x0f
@@ -23,6 +21,8 @@ String orders = "swnex";
 int dir;
 String nodes = "acbba";
 int t;
+int RF_RX_PIN = 6;
+char message[255] = "";
 /*
  * 
  * 
@@ -344,7 +344,18 @@ void nextOrder(){
 }
 
 
-
+  char * getCheckSum(char *string)
+      {
+        int XOR;  
+        for (int i = 0; i < strlen(string); i++) 
+        {
+          XOR += *(string++); 
+        }
+        char xOR[12];
+        sprintf(xOR, "%d", XOR);
+        return xOR; 
+      
+      }
 
 
 
@@ -358,6 +369,9 @@ void setup() {
   delay(2000);
   // put your setup code here, to run once:
   Motor.begin(I2C_ADDRESS);
+  vw_set_rx_pin(RF_RX_PIN);  // Setup receive pin.
+  vw_setup(2000); // Transmission speed in bits per second.
+  vw_rx_start(); // Start the PLL receiver.
   Serial.begin(9600);
   n=0;
   dir = WEST;
@@ -365,6 +379,30 @@ void setup() {
 }
 
 void loop() {
-botChoice();
-
+botChoice(); 
+      uint8_t buf[VW_MAX_MESSAGE_LEN];
+      uint8_t buflen = VW_MAX_MESSAGE_LEN;
+      char *checksum;
+      char *chsum = ""; 
+      int i; 
+      
+       if(vw_get_message(buf, &buflen))
+      {
+        if ((uint8_t)buf[0] == 36 && (uint8_t)buf[1] == 103 && (uint8_t)buf[2] == 49 && (uint8_t)buf[3] == 101 && (uint8_t)buf[4] == 49 )
+        { // vérification de l'entête. 
+                for(i = 8; i < buflen; i++){
+                  strcat(message, (char)buf[i]);
+                  }
+               chsum = getCheckSum((char *)message);
+                for(i = 5 ; i < 8; i++){
+                  strcat(checksum, (char *)buf[i]);
+                  }
+                if(checksum == chsum){
+                   
+                      Serial.print("msg: ");
+                    Serial.println((char *)message);
+                   
+                }
+           } 
+      } 
 }
