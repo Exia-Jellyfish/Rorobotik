5a586feb806d902921c02845e3280b23cd2fbdcb
DrakLulu <charlotte.madroux@gmail.com>
2018-06-26 17:04:53 +0200

    récup vieux code

--- lignesuivie/lignesuivie.ino
+++ lignesuivie/lignesuivie.ino
@@ -1,16 +1,16 @@
 #include "Grove_I2C_Motor_Driver.h"
 
 #define I2C_ADDRESS 0x0f
-#define IRLEFT 7 //left IR sensor
+#define IRLEFT 8 //left IR sensor
 #define LEFTLINE 3 //left IR sensor line follower
 #define RIGHTLINE 4 //right IR sensor line follower
-#define IRRIGHT 6 //right IR sensor
+#define IRRIGHT 7 //right IR sensor
 //MOTOR1 = GAUCHE
 //MOTOR2 = DROITE
 int n;
 #define I2C_ADDRESS 0x0f
-#define cruise 40 //cruise speed
-#define adjust 30  //adjusting trajectory speed
+#define cruise 30 //cruise speed
+#define adjust 25  //adjusting trajectory speed
 /*
  * 
  * 
@@ -42,18 +42,18 @@ goForward();
 }
 void adjustLeft(){
   while (digitalRead(LEFTLINE) == LOW){
-    Motor.speed(MOTOR1, 40);
+    Motor.speed(MOTOR1, adjust);
   // Set speed of MOTOR2, Clockwise
-  Motor.speed(MOTOR2, -40);
+  Motor.speed(MOTOR2, adjust);
   }
 
 }
 
 void adjustRight(){
   while (digitalRead(LEFTLINE) == LOW){
-    Motor.speed(MOTOR1, -40);
+    Motor.speed(MOTOR1, -adjust);
   // Set speed of MOTOR2, Clockwise
-  Motor.speed(MOTOR2, 40);
+  Motor.speed(MOTOR2, adjust);
   
   }
 
@@ -86,29 +86,23 @@ void botChoice(){
   if( digitalRead(IRLEFT) == LOW && digitalRead(LEFTLINE) == HIGH && digitalRead(RIGHTLINE) == HIGH && digitalRead(IRRIGHT) == LOW){ //0110 full forward
     goForward();
     n=0;
-    Serial.println("Forward");
   }
   if( digitalRead(IRLEFT) == LOW && digitalRead(LEFTLINE) == LOW && digitalRead(RIGHTLINE) == HIGH && digitalRead(IRRIGHT) == LOW){ //0010 forward with adjustments needed to the right
     adjustRight(); 
     n=0;
-    Serial.println("adjRight");
   }
   if( digitalRead(IRLEFT) == LOW && digitalRead(LEFTLINE) == HIGH && digitalRead(RIGHTLINE) == LOW && digitalRead(IRRIGHT) == LOW){ //0100
    adjustLeft();
    n=0;
-    Serial.println("adjLeft");
   }
 
   if( digitalRead(IRLEFT) == LOW && digitalRead(LEFTLINE) == LOW && digitalRead(RIGHTLINE) == LOW && digitalRead(IRRIGHT) == HIGH){ // 0001
     rotateRight();
-    Serial.println("tournant à droite"); 
   }
   if( digitalRead(IRLEFT) == HIGH && digitalRead(LEFTLINE) == LOW && digitalRead(RIGHTLINE) == LOW  && digitalRead(IRRIGHT) == LOW  ){ //1000
     rotateLeft();
-    Serial.println("tournant à gauche"); 
   }
    if( digitalRead(IRLEFT) == HIGH && digitalRead(LEFTLINE) == LOW && digitalRead(RIGHTLINE) == LOW && digitalRead(IRRIGHT) == HIGH ){ //1001
-    Serial.println("tournant à gauche ou à droite"); 
     if (millis()%2 ==1){
       rotateLeft();
     }else{
@@ -117,7 +111,6 @@ void botChoice(){
   }
   
   if( digitalRead(IRLEFT) == LOW && digitalRead(LEFTLINE) == HIGH && digitalRead(RIGHTLINE) == HIGH && digitalRead(IRRIGHT) == HIGH){ //0111
-    Serial.println("tournant à droite ou tout droit"); 
     if (millis()%2 ==1){
       goForward();
     }else{
@@ -125,7 +118,6 @@ void botChoice(){
     }
   }
   if( digitalRead(IRLEFT) == HIGH && digitalRead(LEFTLINE) == HIGH && digitalRead(RIGHTLINE) == HIGH && digitalRead(IRRIGHT) == LOW ){ //1110
-    Serial.println("tournant à gauche ou tout droit"); 
    if (millis()%2 ==1){
       goForward();
     }else{
@@ -133,7 +125,6 @@ void botChoice(){
     }
   }
   if( digitalRead(IRLEFT) == HIGH && digitalRead(LEFTLINE) == HIGH && digitalRead(RIGHTLINE) == HIGH  && digitalRead(IRRIGHT) == HIGH ){ //1111
-    Serial.println("tournant à gauche, droite ou tout droit"); 
      switch (random(0-2)){
       case 0:
         goForward();
@@ -149,15 +140,12 @@ void botChoice(){
   }
   
   if( digitalRead(IRLEFT) == LOW && digitalRead(LEFTLINE) == LOW && digitalRead(RIGHTLINE) == HIGH && digitalRead(IRRIGHT) == HIGH  ){ // 0011
-    Serial.println("tournant à droite mais robot décalé à gauche"); 
     rotateRight();
   }
   if( digitalRead(IRLEFT) == HIGH && digitalRead(LEFTLINE) == LOW && digitalRead(RIGHTLINE) == HIGH  && digitalRead(IRRIGHT) == LOW ){ //1000
-    Serial.println("tournant à guauche et robot décalé à gauche."); 
     rotateLeft();
   }
   if( digitalRead(IRLEFT) == HIGH && digitalRead(LEFTLINE) == LOW && digitalRead(RIGHTLINE) == HIGH  && digitalRead(IRRIGHT) == HIGH ){ //1011
-    Serial.println("tournant à droite, gauche et tout droit mais robot décalé à gauche "); 
     switch (random(0-2)){
       case 0:
         goForward();
@@ -173,7 +161,6 @@ void botChoice(){
   }
   
   if( digitalRead(IRLEFT) == LOW  && digitalRead(LEFTLINE) == HIGH && digitalRead(RIGHTLINE) == LOW && digitalRead(IRRIGHT) == HIGH ){ //0101
-    Serial.println("tournant à droite et tout droit et robot décalé sur la droite "); 
      if (millis()%2 ==1){
       goForward();
     }else{
@@ -181,7 +168,6 @@ void botChoice(){
     }
   }
   if( digitalRead(IRLEFT) == HIGH && digitalRead(LEFTLINE) == HIGH && digitalRead(RIGHTLINE) == LOW &&  digitalRead(IRRIGHT) == LOW ){ //0101
-    Serial.println("tournant à gauche et tout droit et robot décalé sur la droite"); 
     if (millis()%2 ==1){
       rotateLeft();
     }else{
@@ -189,7 +175,6 @@ void botChoice(){
     }
   }
    if( digitalRead(IRLEFT) == HIGH && digitalRead(LEFTLINE) == HIGH && digitalRead(RIGHTLINE) == LOW &&  digitalRead(IRRIGHT) == HIGH ){ //1101
-    Serial.println("tournant à droite, gauche et tout droit et robot décalé sur la droite ");
     switch (random(0-2)){
       case 0:
         goForward();
@@ -222,7 +207,7 @@ void setup() {
 
 void loop() {
   // put your main code here, to run repeatedly
-  botChoice();
+  //botChoice();
    
   
 }
